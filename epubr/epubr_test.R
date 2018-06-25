758a4730493c09d735fef43f957e5d2ffc52ec25
suzan baert <suzan.baert@outlook.com>
2018-06-25 20:18:51 +0200

    first script

--- epubr/epubr_test.R
+++ epubr/epubr_test.R
@@ -1,1 +1,10 @@
 library(epubr)
+
+
+file <- system.file("testfiles/HTGAJ.epub", package = "epubr")
+HTGAJ <- epub(file)
+  
+#error
+#Error in if (grepl("<|>", x)) { : argument is of length zero
+# In addition: Warning message:
+# In utils::unzip(file, exdir = exdir) : error 1 in extracting from zip file
