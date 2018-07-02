0b723b7367787d617b134779e1a298cd6b875dea
suzan baert <suzan.baert@outlook.com>
2018-07-02 17:07:33 +0200

    adding to review

--- epubr/epubr_test.R
+++ epubr/epubr_test.R
@@ -1,13 +1,44 @@
 library(epubr)
+library(dplyr)
 
 
 #SHARED EXAMPLE
 file <- system.file("dracula.epub", package = "epubr")
-
-
-#functions test
-dracula_meta <- epub_meta(file)
 dracula <- epub(file)
+
+
+
+### FUNCTIONS TEST ####
+
+
+#epub_meta
+dracula_meta <- epub_meta(file)
+
+epub_meta(file) %>% 
+  mutate_all(toupper) %>% 
+  select(identifier)
+
+
+
+#epub_unzip
+unziptest <- epub_unzip(file)
+unziptest <- epub_unzip(file, exdir = "./epubr/unzip")
+
+
+
+#first_nchar
+firstchar <- first_nchar(file)
+firstchar <- first_nchar(file, n=10)
+
+
+#epub
+dracula <- epub(file)
+
+
+
+#pipe-able
+epub_meta(file) %>% 
+  mutate_all(toupper)
 
 
 #extracting text
